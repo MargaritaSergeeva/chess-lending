87155418eaa54f8e7ec9e62d4a8fde8c950140dc
margarita_sergeeva <margaritk@inbox.ru>
2026-05-16 22:42:14 +0300

    Add page scaling for narrow screens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
+import { initPageScale } from "./modules/page-scale.js";
 import { initStagesSlider } from "./modules/stages-slider.js";
 import { initParticipantsSlider } from "./modules/participants-slider.js";
 
+initPageScale();
 initStagesSlider();
 initParticipantsSlider();
