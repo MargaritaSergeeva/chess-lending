c9a227b20bad0996674a7e09d729b72a527f3f94
margarita_sergeeva <margaritk@inbox.ru>
2026-05-17 12:35:35 +0300

    Add subtle reveal animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 import { initPageScale } from "./modules/page-scale.js";
+import { initRevealAnimations } from "./modules/reveal-animations.js";
 import { initRunningLines } from "./modules/running-line.js";
 import { initStagesSlider } from "./modules/stages-slider.js";
 import { initParticipantsSlider } from "./modules/participants-slider.js";
 
 initRunningLines();
 initPageScale();
+initRevealAnimations();
 initStagesSlider();
 initParticipantsSlider();
